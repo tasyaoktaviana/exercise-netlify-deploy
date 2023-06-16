0d117c396a11ba30956f13b328ab5fe5d9e0c6c9
tasyaoktaviana <89627474+tasyaoktaviana@users.noreply.github.com>
2023-06-16 12:45:47 +0700

    Add files via upload

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL" // TODO: replace this
+const studentName = "Tasya Oktaviana Dwi Cahyanti" // TODO: replace this
+const studentId = "FE4300716" // TODO: replace this
+
+export { url, studentName, studentId }
